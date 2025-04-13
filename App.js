c69e11505025a762ffb169f4681adbd6d5db9801
volkovajuls <volkovajs.work@mail.ru>
2025-04-13 16:45:41 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import * as React from 'react';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as React from "react";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import Breeds from './src/screens/Breeds/index';
-import AboutBreed from './src/screens/AboutBreed/index';
-import Favorites from  './src/screens/Favorites/index'
+import Breeds from "./src/screens/Breeds/index";
+import AboutBreed from "./src/screens/AboutBreed/index";
+import Favorites from "./src/screens/Favorites/index";
 
 const Stack = createStackNavigator();
 function MainStack() {
@@ -17,7 +17,6 @@ function MainStack() {
     >
       <Stack.Screen name="Breeds" component={Breeds} />
       <Stack.Screen name="AboutBreed" component={AboutBreed} />
-      <Stack.Screen name="Favorites" component={Favorites} />
     </Stack.Navigator>
   );
 }
