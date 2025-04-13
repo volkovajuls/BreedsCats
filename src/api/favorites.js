f73cbfe91225615aca49fe0072e7c1225499cbc6
volkovajuls <volkovajs.work@mail.ru>
2025-04-13 16:47:35 +0300

    newRequest

--- src/api/favorites.js
+++ src/api/favorites.js
@@ -1,26 +1,49 @@
-import { postRequest,getRequest } from './index';
-import { ApiUrls } from './urls';
+import { postRequest, getRequest, deleteRequest } from "./index";
+import { ApiUrls } from "./urls";
 
-export const saveImageInFavorites = async (imageId) => {
-    const url = ApiUrls.favorites;
+export const saveImageInFavoritesApi = async (imageId) => {
+  const url = `${ApiUrls.favorites}`;
+  try {
+    const response = await postRequest(url, { image_id: imageId });
+    // console.log("responsePost ", response.data["message"]);
+    return response.data["message"] === "SUCCESS";
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
 
-    try {
-      const response = await postRequest(url, { image_id: imageId });
+export const loadImageInFavoritesApi = async (imageId) => {
+  const url = `${ApiUrls.favorites}`;
 
-    } catch (error) {
-      console.log(error);
-      return [];
-    }
-}
+  try {
+    const response = await getRequest(url, { image_id: imageId });
+    // console.log("loadimageFav ", response?.data);
+    return response?.data ?? [];
+  } catch (error) {
+    console.log("load", error);
+  }
+};
 
-export const loadFavorites = async () => {
-    const url = ApiUrls.favorites;
+export const loadFavoritesApi = async () => {
+  const url = `${ApiUrls.favorites}`;
 
-    try {
-      const response = await getRequest(url);
-      return response?.data ?? [];
-    } catch (error) {
-      console.log(error);
-      return [];
-    }
-  };
+  try {
+    const response = await getRequest(url);
+    // console.log(response?.data);
+    return response?.data ?? [];
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+export const deleteImageFromFavoritesApi = async (favoritesId) => {
+  const url = `${ApiUrls.favorites}/${favoritesId}`;
+  try {
+    const response = await deleteRequest(url);
+    return response.data["message"] === "SUCCESS";
+  } catch (error) {
+    console.log("delete", error);
+  }
+};
